e77247fda7544709360b02799ce82d3361e9c09c
X-Noid <xgorntv@gmail.com>
2021-07-20 13:25:50 +0900

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton
 
 class Translation(object):
     START_TEXT = "Hi {},\nI'm MangaLoader!\nYou can upload any manga to Telegram as Zipfile!\n\n/help for more details!"
-    HELP_TEXT = "How to use MangaLoader?!\n\n1. Sent manga url from [supported resources](https://github.com/X-Gorn/MangaLoader/tree/cleandir#url-options-and-examples).\n2. Choose subdir option. (will uploaded as Zipfile)\n3. Wait a few minutes and you will recieve your archived manga\n\nIf bot didn't respond, contact @xgorn"
+    HELP_TEXT = "How to use MangaLoader?!\n\n1. Sent manga url from [supported resources](https://github.com/X-Gorn/MangaLoader/tree/cleandir#url-options-and-examples).\n2. Choose subdir option. (will uploaded as Zipfile)\n3. Wait a few minutes and you will receive your archived manga\n\nIf bot didn't respond, contact @xgorn"
     UPLOAD = "Choose subdirs option. (will uploaded as Zipfile)"
     CLEANDIR_SUCCESS = 'Successfully cleaned your download dir, now you can sent another link'
     CLEANDIR_UNSUCCESS = 'Your download dir are empty, use this command only if your bot are stuck'
